aff6a2eb977d3abdc235b6f8936778ddea88d7e1
Shaw <xiaodigen@vspn.com>
2021-07-21 17:28:59 +0800

    ✨ feat(dio): 添加yapi Get请求

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,18 +3,80 @@
  * @Date: 2021-07-21 14:44:47
  * @Description: 
  * @LastEditors: Shaw
- * @LastEditTime: 2021-07-21 15:02:24
+ * @LastEditTime: 2021-07-21 17:28:06
  */
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<HomePage> {
+  TextEditingController typeController = TextEditingController();
+  String showText = '这里展示搜索结果';
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('首页'),
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('商城首页'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: typeController,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                labelText: '物品类型',
+                helperText: '请输入你想查找的内容',
+              ),
+              autofocus: false,
+            ),
+            ElevatedButton(
+              onPressed: _choiceAction,
+              child: const Text('点击查找'),
+            ),
+            Text(
+              showText,
+              // overflow: TextOverflow.ellipsis,
+              // maxLines: 3,
+            )
+          ],
+          // ignore: deprecated_member_use
+        ),
+      ),
+    );
+  }
+
+  void _choiceAction() {
+    print('开始查询...');
+    if (typeController.text.toString() == '') {
+      showDialog(
+          context: context,
+          builder: (context) => const AlertDialog(title: Text('不能为空')));
+    } else {
+      getHttp(typeController.text.toString()).then((val) {
+        setState(() {
+          showText = val['data'].toString();
+        });
+      });
+    }
+  }
+
+  Future getHttp(String typeText) async {
+    try {
+      print('参数:' + typeText);
+      var data = {'name': typeText};
+      Response response = await Dio().get(
+          "http://192.168.0.11:3001/mock/37/getUsers",
+          queryParameters: data);
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
