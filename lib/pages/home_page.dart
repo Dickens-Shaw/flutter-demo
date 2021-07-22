0ac272affe01d659af09bd9fd003a8a2e2a8ee87
Shaw <xiaodigen@vspn.com>
2021-07-22 16:44:04 +0800

    ✨ feat():

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,41 +1,62 @@
-/*
- * @Author: Shaw
- * @Date: 2021-07-21 14:44:47
- * @Description: 
- * @LastEditors: Shaw
- * @LastEditTime: 2021-07-22 14:00:48
- */
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomepageState createState() => _HomepageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomepageState extends State<HomePage> {
-  String homePageContent = '正在获取数据';
-  @override
-  void initState() {
-    getHomePageContent().then((val) {
-      print(val);
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('百姓生活+'),
         ),
-        body: SingleChildScrollView(
-          child: Text(homePageContent),
+        body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var data = json.decode(snapshot.data.toString());
+              List<Map> swiperDataList =
+                  (data['data'] as List).cast(); // 顶部轮播组件数
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDataList: swiperDataList), //页面顶部轮播组件
+                ],
+              );
+            } else {
+              return const Center(
+                child: Text('加载中'),
+              );
+            }
+          },
         ));
+  }
+}
+
+// 首页轮播组件编写
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  const SwiperDiy({Key? key, required this.swiperDataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 333.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network("${swiperDataList[index]['image']}",
+              fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: const SwiperPagination(),
+        autoplay: true,
+      ),
+    );
   }
 }
